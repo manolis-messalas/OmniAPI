b47d3093cfffe5618ef421974b72c5aba28e8c29
Messalas <emessalas@cgs-online.de>
2024-10-25 13:38:14 +0300

    Initial commit

---
+++ src/main/java/com/messalas/spring_boot_demo_A/SpringBootDemoAApplication.java
@@ -0,0 +1,13 @@
+package com.messalas.spring_boot_demo_A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/messalas/spring_boot_demo_A/SpringBootDemoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messalas.spring_boot_demo_A;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
