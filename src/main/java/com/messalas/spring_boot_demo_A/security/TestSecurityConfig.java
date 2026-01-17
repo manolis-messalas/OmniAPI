f57568f7b7fe512f038c6ad7f407e397a09635c6
manolis-messalas <messalasmanwlis@gmail.com>
2026-01-17 20:53:10 +0200

    Add test security config

--- src/main/java/com/messalas/spring_boot_demo_A/security/TestSecurityConfig.java
+++ src/main/java/com/messalas/spring_boot_demo_A/security/TestSecurityConfig.java
@@ -2,23 +2,24 @@ package com.messalas.spring_boot_demo_A.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@Profile("test")
+public class TestSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/health").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
